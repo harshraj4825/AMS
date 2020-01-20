69e64d6549862fd0a3f99e7a71204790d23212f2
harshraj4825 <iitianharshraj@gmail.com>
2020-01-20 13:20:39 +0530

    app connected to our server through retrofit interface

---
+++ app/src/main/java/thezero/pkd/ams/Retrofit/Retrofit_models/LoginStudentResult.java
@@ -0,0 +1,14 @@
+package thezero.pkd.ams.Retrofit.Retrofit_models;
+
+public class LoginStudentResult {
+    private String Name;
+    private Integer UserId;
+
+    public String getName() {
+        return Name;
+    }
+
+    public Integer getUserId() {
+        return UserId;
+    }
+}
