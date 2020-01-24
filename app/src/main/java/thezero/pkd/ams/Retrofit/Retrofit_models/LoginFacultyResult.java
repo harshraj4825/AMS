d28c435797da0dd4403a16cc16f066c996d3dfbf
harshraj4825 <iitianharshraj@gmail.com>
2020-01-24 22:08:27 +0530

    faculty login from server

---
+++ app/src/main/java/thezero/pkd/ams/Retrofit/Retrofit_models/LoginFacultyResult.java
@@ -0,0 +1,14 @@
+package thezero.pkd.ams.Retrofit.Retrofit_models;
+
+public class LoginFacultyResult {
+    private String Name;
+    private String  Email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getUserId() {
+        return Email;
+    }
+}
