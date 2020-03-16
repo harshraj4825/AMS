98937d742d65e6413fb001564c56327ee1dac56a
harshraj4825 <iitianharshraj@gmail.com>
2020-03-16 18:58:08 +0530

    Now student/faculty can download attendance report for specific student

--- app/src/main/java/thezero/pkd/ams/Students/Student_attendance_Information.java
+++ app/src/main/java/thezero/pkd/ams/Students/Student_attendance_Information.java
@@ -1,18 +1,34 @@
 package thezero.pkd.ams.Students;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.JsonObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
@@ -28,7 +44,7 @@ import thezero.pkd.ams.Retrofit.Retrofit_models.S_A_I_Result;
 import thezero.pkd.ams.utils.User;
 
 public class Student_attendance_Information extends Activity {
-    private String roll_no,name,course_code;
+    private String roll_no,name,course_code,t_present,t_lecture;
     private TextView tRoll,tName,tCode,tTotal_lec,tTotal_atten;
     private RecyclerView recyclerView;
     private RetrofitRoutesInterface retrofitRoutesInterface;
@@ -70,6 +86,16 @@ public class Student_attendance_Information extends Activity {
         layoutManager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        FloatingActionButton fab = findViewById(R.id.stu_float);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(Student_attendance_Information.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+                createPDf();
+            }
+        });
+        //fab.setOnClickListener(view -> Toast.makeText(Student_attendance_Information.this,"working fine",Toast.LENGTH_SHORT).show());
+
         retrofitRoutesInterface= ClientApi.getInstance().getApi();
         JsonObject jsonObject=new JsonObject();
         jsonObject.addProperty("course_code",course_code);
@@ -80,6 +106,8 @@ public class Student_attendance_Information extends Activity {
             @Override
             public void onResponse(Call <S_A_I_Result> call, Response <S_A_I_Result> response) {
                 if (response.code()==200){
+                    t_lecture=response.body().getTotal_lacture();
+                    t_present=response.body().getTotal_Present();
                     tTotal_lec.setText(response.body().getTotal_lacture());
                     tTotal_atten.setText(response.body().getTotal_Present());
                     s_a_i_recyclerViewLists=response.body().getS_a_i_recyclerViewLists();
@@ -99,5 +127,99 @@ public class Student_attendance_Information extends Activity {
             }
         });
 
+    }
+    public void createPDf(){
+        int pageWidth=595;
+        int pageHeight=842;
+        int Course_code_size=22;
+        int pdfFont=18;
+        int lrBorder= 30;
+        PdfDocument document = new PdfDocument();
+        // crate a page description
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, 1).create();
+        // start a page
+        PdfDocument.Page page = document.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+        Paint paint=new Paint();
+
+        paint.setColor(Color.BLACK);
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.BOLD_ITALIC));
+
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setTextSize(Course_code_size);
+        canvas.drawText(course_code,pageWidth/2,35,paint);
+
+        paint.setTextAlign(Paint.Align.RIGHT);
+        canvas.drawText(name,pageWidth-lrBorder,55,paint);
+        canvas.drawText("Total Present:-"+t_present,pageWidth-lrBorder,80,paint);
+
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText(roll_no,lrBorder,55,paint);
+        canvas.drawText("Total lectures:-"+t_lecture,lrBorder,80,paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2);
+        float lineGap=(float) (pageWidth-2*lrBorder)/2;
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.NORMAL));
+
+        int y_height_Line=95;
+        canvas.drawLine(lrBorder,y_height_Line,pageWidth-lrBorder,y_height_Line,paint);
+        canvas.drawLine(lrBorder,pageHeight-lrBorder,pageWidth-lrBorder,pageHeight-lrBorder,paint);
+        for(int j=0;j<=2;j++){
+            canvas.drawLine(lrBorder+lineGap*j,y_height_Line,lrBorder+lineGap*j,pageHeight-lrBorder,paint);
+        }
+        int height=y_height_Line+30;
+        int rheight=height;
+        int x_coordinate=lrBorder+20;
+        int space_bw_two=23;
+        for (int i=1;i<=s_a_i_recyclerViewLists.size();i++){
+            Paint paint1=new Paint();
+            paint1.setTextAlign(Paint.Align.LEFT);
+            paint1.setTextSize(pdfFont);
+            paint1.setColor(Color.BLACK);
+            if (height<=pageHeight-lrBorder){
+                if(s_a_i_recyclerViewLists.get(i-1).getStatus().equals("Present")){
+                    canvas.drawText(String.valueOf(i),x_coordinate,height,paint1);
+                    canvas.drawText(s_a_i_recyclerViewLists.get(i-1).getDate(),x_coordinate+35,height,paint1);
+                    canvas.drawText("1",x_coordinate+150,height,paint1);
+                }else {
+                    paint1.setColor(Color.RED);
+                    canvas.drawText(String.valueOf(i),x_coordinate,height,paint1);
+                    canvas.drawText("0",x_coordinate+150,height,paint1);
+                    canvas.drawText(s_a_i_recyclerViewLists.get(i-1).getDate(),x_coordinate+35,height,paint1);
+                }
+                height=height+space_bw_two;
+            }else if(rheight<pageHeight-lrBorder){
+                if(s_a_i_recyclerViewLists.get(i-1).getStatus().equals("Present")){
+                    canvas.drawText(String.valueOf(i),x_coordinate+lineGap,height,paint1);
+                    canvas.drawText(s_a_i_recyclerViewLists.get(i-1).getDate(),x_coordinate+lineGap+35,height,paint1);
+                    canvas.drawText("1",x_coordinate+lineGap+150,height,paint1);
+                }else {
+                    paint1.setColor(Color.RED);
+                    canvas.drawText(String.valueOf(i),x_coordinate+lineGap,height,paint1);
+                    canvas.drawText("0",x_coordinate+lineGap+150,height,paint1);
+                    canvas.drawText(s_a_i_recyclerViewLists.get(i-1).getDate(),x_coordinate+lineGap+35,height,paint1);
+                }
+                rheight=rheight+space_bw_two;
+            }
+        }
+        canvas.drawLine(28,44,38,44,paint);
+
+        document.finishPage(page);
+        String directory_path = Environment.getExternalStorageDirectory().getPath() + "/AMS/";
+        File file = new File(directory_path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String targetPdf = directory_path+name+"-attendance-report.pdf";
+        File filePath = new File(targetPdf);
+        try {
+            document.writeTo(new FileOutputStream(filePath));
+            Toast.makeText(this, "Save at "+directory_path, Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Log.e("main", "error "+e.toString());
+            Toast.makeText(this, "Something wrong: " + e.toString(),  Toast.LENGTH_LONG).show();
+        }
+        document.close();
     }
 }
